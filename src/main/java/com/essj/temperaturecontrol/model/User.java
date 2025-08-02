fb0ee38be3d21d7e80d9a58773e4b6fbeeca9b61
Evandro Sacramento II <evandro-dev@outlook.com>
2025-08-02 15:47:58 -0300

    Classe User.java

---
+++ src/main/java/com/essj/temperaturecontrol/model/User.java
@@ -0,0 +1,74 @@
+package com.essj.temperaturecontrol.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class User {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String nome;
+    private String password;
+    private LocalDateTime cadastro = LocalDateTime.now();
+
+
+    public User() {
+    }
+
+    public User(Long id, String email, String nome, String password, LocalDateTime cadastro) {
+        this.id = id;
+        this.email = email;
+        this.nome = nome;
+        this.password = password;
+        this.cadastro = cadastro;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getCadastro() {
+        return cadastro;
+    }
+
+    public void setCadastro(LocalDateTime cadastro) {
+        this.cadastro = cadastro;
+    }
+}
