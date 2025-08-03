4342ddff405b408a50d4bf20ec79fcb1e296919e
Evandro Sacramento II <evandro-dev@outlook.com>
2025-08-03 17:37:30 -0300

    Criação de token para segurança de dados

---
+++ src/main/java/com/essj/temperaturecontrol/security/TokenFilter.java
@@ -0,0 +1,42 @@
+package com.essj.temperaturecontrol.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Set;
+
+
+@Component
+public class TokenFilter extends OncePerRequestFilter {
+
+    private static final Set<String> TOKENS_ESP = Set.of("e1ffe7cd-7115-4748-b6c6-372d99f562b8");
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        String path = request.getServletPath();
+
+        if ("/historico".equals(path) || "/tempatual".equals(path)){
+            String token = request.getHeader("Authorization");
+            if (token == null || !token.startsWith("Bearer ")) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                return;
+            }
+
+            String tokenRecebido = token.substring("Bearer ".length());
+            if (!TOKENS_ESP.contains(tokenRecebido)) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                return;
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
